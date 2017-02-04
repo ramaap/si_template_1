25a6bbe7741e4801f4fe480162bbf1aa20e01e50
ramaap <rama.adyasa@gmail.com>
2017-02-04 14:19:46 +0700

    adding new page

--- application/views/front/pages/contact.php
+++ application/views/front/pages/contact.php
@@ -6,6 +6,8 @@
 
 	<div class="main container">
 		<h2>Contact</h2>
+		<p>Hallo! Terima kasih sudah menjadi pelanggan setia kami. Apabila ada pertanyaan, silahkan cek halaman FAQ (Frequently Asked Question) terlebih dahulu. Bila pertanyaanmu belum terjawab di halaman tersebut, silakan kirim email kepada kami dengan mengisi form dibawah ini.</p>
+	
 	</div>
 
 
