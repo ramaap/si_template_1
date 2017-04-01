1c6021626be83611f33c84bdc844817c28aec1ba
ramaap <rama.adyasa@gmail.com>
2017-04-01 18:57:30 +0700

    fix catalog dan homepage

--- application/views/front/pages/catalog_print.php
+++ application/views/front/pages/catalog_print.php
@@ -8,61 +8,60 @@
 	<?php $this->load->view('front/slice/carousel_pixaprint'); ?>
 		<div id="ourproduct">
 			<h2>OUR PRODUCT</h2>
-			<div class="ourproduct-container">
+		</div> 
+	</div>
+				<div class="ourproduct-container">
 				<div class="row">
 				  <div class="col-sm-4">
 					<div class="image-container">
 						<a href="<?php echo site_url("front/checkout/product_detail_business_card/"); ?>">
 							<img src="<?php echo base_url(); ?>include/front/images/index/pixaprint/busines_card.png">
-						</a>
 							<div><p>Business Card</p></div>
+						</a>
 					</div>
 				  </div>
 				  <div class="col-sm-4">
 					<div class="image-container">
 						<a href="<?php echo site_url("front/checkout/product_detail_flyer/"); ?>">
 							<img src="<?php echo base_url(); ?>include/front/images/index/pixaprint/flyer.png">
-						</a>
 							<div><p>Flyer</p></div>
+						</a>
 					</div>
 				  </div>
 				  <div class="col-sm-4">
 					<div class="image-container">
 						<a href="<?php echo site_url("front/checkout/product_detail_brochure/"); ?>">
 							<img src="<?php echo base_url(); ?>include/front/images/index/pixaprint/brochure.png">
-						</a>
 							<div><p>Brochure</p></div>
+						</a>
 					</div>
 				  </div>
 				  <div class="col-sm-4">
 					<div class="image-container">
 						<a href="<?php echo site_url("front/checkout/product_detail_poster/"); ?>">
 							<img src="<?php echo base_url(); ?>include/front/images/index/pixaprint/poster.png">
-						</a>
 							<div><p>Poster</p></div>
+						</a>
 					</div>
 				  </div>
 				  <div class="col-sm-4">
 					<div class="image-container">
 						<a href="<?php echo site_url("front/checkout/product_detail_kop/"); ?>">
 							<img src="<?php echo base_url(); ?>include/front/images/index/pixaprint/kop_surat.png">
-						</a>
 							<div><p>Kop Surat</p></div>
+						</a>
 					</div>
 				  </div>
 				  <div class="col-sm-4">
 					<div class="image-container">
 						<a href="<?php echo site_url("front/checkout/product_detail_amplop/"); ?>">
 							<img src="<?php echo base_url(); ?>include/front/images/index/pixaprint/amplop.png">
-						</a>
 							<div><p>Amplop</p></div>
+						</a>
 					</div>
 				  </div>
 				</div>
 			</div>
-		</div> 
-	</div>
-
         <?php $this->load->view('front/slice/footer'); ?>
 </body>
 </html>
